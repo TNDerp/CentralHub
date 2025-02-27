8d0595eec3eb2ed6b38f4b169ebd3576a54747c8
TNDerp <tnderp@gmail.com>
2025-02-27 10:37:34 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Simple mobile menu toggle
+      const menuToggle = document.getElementById("mobile-menu");
+      const navList = document.getElementById("nav-list");
+
+      menuToggle.addEventListener("click", () => {
+        navList.classList.toggle("active");
+});
+    
